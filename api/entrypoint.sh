10d662122de5e4f2b157e406a5e82aa66b25220f
shafi <mehedishafi@hotmail.com>
2022-02-16 09:23:00 +0600

    devops: add dockerfile and compose config for api

---
+++ api/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+until cd /app/api/
+do
+    echo "Waiting for server volume ..."
+done
+
+until python manage.py migrate
+do
+    echo "Waiting for db to be ready ..."
+    sleep 2
+done
+
+python manage.py collectstatic --noinput
+
+# you can use either. its no big deal
+gunicorn api.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
+# python manage.py runserver 0.0.0.0:8000
