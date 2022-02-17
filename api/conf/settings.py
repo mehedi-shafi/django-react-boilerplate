e0dc4915e62ad449f01ba5f55832f4dab5cd9e81
shafi <mehedishafi@hotmail.com>
2022-02-17 11:38:02 +0600

    feat: docker-compose config for frontend. fix: static url for backend

--- api/conf/settings.py
+++ api/conf/settings.py
@@ -173,12 +173,4 @@ SETTINGS_EXPORT = [
     "API_BROWSER_HEADER",
 ]
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
