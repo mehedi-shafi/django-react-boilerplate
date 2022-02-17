afaa2dd5dfa7836c80fb346582f058a7907eac5d
shafi <mehedishafi@hotmail.com>
2022-02-17 11:06:09 +0600

    feat: add database service to docker-compose with init script chore: reposition environment copy in backend dockerfile

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -14,5 +14,5 @@ done
 python manage.py collectstatic --noinput
 
 # you can use either. its no big deal
-gunicorn api.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
+gunicorn conf.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
 # python manage.py runserver 0.0.0.0:8000
